d1ba6a2fb35bb9ff70697e27a688fd72818876e1
tinnast <tis12@hi.is>
2019-02-25 11:13:25 +0000

    Modelz

---
+++ app/src/main/java/is/landsbankinn/eta/utils/SearchParam.java
@@ -0,0 +1,44 @@
+package is.landsbankinn.eta.utils;
+
+
+import java.util.List;
+
+public class SearchParam {
+    private String name;
+    private List<String> tags;
+    private String genre;
+    private int price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ app/src/main/java/is/landsbankinn/eta/utils/User.java
@@ -0,0 +1,51 @@
+package is.landsbankinn.eta.utils;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private String type;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
